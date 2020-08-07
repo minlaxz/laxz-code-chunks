19d6fa252a5f932a0a0780a53cb8a51aafbac258
minlaxz <mgmglatt.6991@gmail.com>
2020-08-08 01:22:37 +0630

    network chunk's guide.

---
+++ pihole-webhook-ifttt/blockdomains.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+
+pihole --regex '(^|\.)Hulu\.com$' '(^|\.)Netflix\.com$' '(^|\.)disneyplus\.com$' '(^|\.)primevideo\.com$' '(^|\.)roblox\.com$'
---
+++ pihole-webhook-ifttt/unblockdomains.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pihole -b -d --regex '(^|\.)Hulu\.com$' '(^|\.)Netflix\.com$' '(^|\.)disneyplus\.com$' '(^|\.)primevideo\.com$' '(^|\.)roblox\.com$'
+
